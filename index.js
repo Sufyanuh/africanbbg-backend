eb11564d6fd2ef032a3ffd1d0ed543c771aeff01
sufyanulhaq <sufyanulhaq@hnhtechsolutions.com>
2025-01-04 20:58:03 +0500

    done

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ const PORT = 8003;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // MiddlesWares
+// Allow Cors on All domains
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

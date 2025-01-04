edec12737ded87f2b1061301efde664525cda943
Sufyan <100311520+Sufyanuh@users.noreply.github.com>
2025-01-04 10:36:03 +0000

    changes

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { ConnectMongoDB } from "./connection.js";
-import { loginAdmin } from "./controllers/admin/auth.js";
-import seedAdmin from "./seed/seedadmin.js";
-import { productRoutesAdmin } from "./routes/admin/product.js";
-import { checkAuthToken } from "./middlewares/auth.js";
+import { ConnectMongoDB } from "../connection.js";
+import { loginAdmin } from "../controllers/admin/auth.js";
+import seedAdmin from "../seed/seedadmin.js";
+import { productRoutesAdmin } from "../routes/admin/product.js";
+import { checkAuthToken } from "../middlewares/auth.js";
 
 const app = express();
 const PORT = 8003;

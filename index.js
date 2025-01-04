7f0bc8bb6683321fbcaf3c0de51ad23277d7f5db
sufyanulhaq <sufyanulhaq@hnhtechsolutions.com>
2025-01-04 21:07:11 +0500

    allow cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middlewares
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
